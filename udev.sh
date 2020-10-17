6515f289dbc8a5c7364c7f8b10ca9d2f1494c730
WhiteWhidow <whitewhidow@users.noreply.github.com>
2020-10-17 14:00:14 +0200

    Update udev.sh

--- udev.sh
+++ udev.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"2833\", ATTR{idProduct}==\"0186\", MODE=\"0666\", OWNER=\"$1\"" >> /etc/udev/rules.d/51-android.rules
+udevadm control --reload-rule
