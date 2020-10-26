48b7d70fd435303be0f168afce158b9a2ba0db21
sam <contact@branchbit.be>
2020-10-26 09:07:18 +0100

    FIX_INSTALL

--- install.sh
+++ install.sh
@@ -51,6 +51,7 @@ fi
 echo "Checking adb."
 if [[ $(which adb) != *"adb"* ]]; then
   echo "Installing adb."
+  mkdir -p ${OSTYPE}_adb_lib
   curl --silent https://raw.githubusercontent.com/whitewhidow/quest-sideloader-linux/main/${OSTYPE}_adb_lib/adb -o ${OSTYPE}_adb_lib/adb > /dev/null
   sudo cp ${OSTYPE}_adb_lib/adb /usr/local/bin && echo "adb copied from ${OSTYPE}_adb_lib/adb to /usr/local/bin."
 fi
@@ -59,6 +60,7 @@ echo "Adb installed"
 echo "Checking aapt."
 if [[ $(which aapt) != *"aapt"* ]]; then
   echo "Installing aapt."
+  mkdir -p ${OSTYPE}_aapt_lib
   curl --silent https://raw.githubusercontent.com/whitewhidow/quest-sideloader-linux/main/${OSTYPE}_aapt_lib/aapt -o ${OSTYPE}_aapt_lib/aapt > /dev/null
   sudo cp ${OSTYPE}_aapt_lib/aapt /usr/local/bin && echo "aapt copied from ${OSTYPE}_aapt_lib/adb to /usr/local/bin."
 fi
