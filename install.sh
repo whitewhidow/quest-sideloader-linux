93f661f98d80ac8e08eef34c040d9349760bfc0d
sam <contact@branchbit.be>
2020-10-26 21:37:52 +0100

    rcloneoutput

--- install.sh
+++ install.sh
@@ -96,7 +96,8 @@ echo "Zenity installed"
 echo 	"Checking rclone."
 if [[ $(which rclone) != *"rclone"* ]]; then
   echo "Attempting to install missing 'rclone' paackage. (requires sudo)"
-  curl --silent https://rclone.org/install.sh | sudo bash
+  curl --silent https://rclone.org/install.sh -o rcloneinstall.sh > /dev/null
+  sudo ./rcloneinstall.sh > /dev/null
 fi
 echo "Rclone installed"
 
