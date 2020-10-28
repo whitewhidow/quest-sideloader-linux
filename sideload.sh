bd10242c6446a525479667ac5a27550ffe983700
sam <contact@branchbit.be>
2020-10-28 02:36:00 +0100

    FIX

--- sideload.sh
+++ sideload.sh
@@ -106,13 +106,16 @@ ok "ADB path set to: \"$ADB\""
 
 ## AAPT INSTALL
 info "SEPERATE AAPT Detection"
+
 if [ $OSTYPE == "mac" ]; then
-    warning "PLEASE INSTALL aapt from androidaapt.com, WE WILL JUST DOWNLOAD LOCALLY FOR NOW, NO WORRIES !"
-    info "DOWNLOADING https://raw.githubusercontent.com/whitewhidow/quest-sideloader-linux/main/mac_aapt_lib/aapt"
-    curl -s https://raw.githubusercontent.com/whitewhidow/quest-sideloader-linux/main/mac_aapt_lib/aapt -o aapt
-    chmod +x ./aapt
-    AAPT="./aapt"
-    warning "PLEASE INSTALL aapt from androidaapt.com to avoid this download in the future !!"
+    if ! command -v aapt &> /dev/null; then	
+	    warning "PLEASE INSTALL aapt from androidaapt.com, WE WILL JUST DOWNLOAD LOCALLY FOR NOW, NO WORRIES !"
+	    info "DOWNLOADING https://raw.githubusercontent.com/whitewhidow/quest-sideloader-linux/main/mac_aapt_lib/aapt"
+	    curl -s https://raw.githubusercontent.com/whitewhidow/quest-sideloader-linux/main/mac_aapt_lib/aapt -o aapt
+	    chmod +x ./aapt
+	    AAPT="./aapt"
+	    warning "PLEASE INSTALL aapt from androidaapt.com to avoid this download in the future !!"
+    fi
 fi
 ok "AAPT path set to: \"$AAPT\""
 ## END AAPT INSTALL
